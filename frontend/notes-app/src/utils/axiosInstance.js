6bb6488cd4e87e93ad83991e42946c06ddb6bd42
qemal <gembol1419@gmail.com>
2025-07-11 10:46:04 +0700

    testing url

--- frontend/notes-app/src/utils/axiosInstance.js
+++ frontend/notes-app/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { BASE_URL } from "./constants";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "http://ec2-13-236-119-165.ap-southeast-2.compute.amazonaws.com/",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
